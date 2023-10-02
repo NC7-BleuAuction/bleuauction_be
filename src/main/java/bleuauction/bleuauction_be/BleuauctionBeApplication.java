f4867f080854cfa1b7a9598498839ccb5a8412ba
wooki37 <lite1324@naver.com>
2023-10-02 16:03:04 +0900

    initial/be

---
+++ src/main/java/bleuauction/bleuauction_be/BleuauctionBeApplication.java
@@ -0,0 +1,13 @@
+package bleuauction.bleuauction_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BleuauctionBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BleuauctionBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bleuauction/bleuauction_be/BleuauctionBeApplicationTests.java
@@ -0,0 +1,13 @@
+package bleuauction.bleuauction_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BleuauctionBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

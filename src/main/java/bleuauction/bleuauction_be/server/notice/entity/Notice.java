05575dfe4153c3db5847d11ee2c148dc0d27932d
seunghyun333 <shb2in33@naver.com>
2023-10-06 02:43:14 +0900

    공지사항 구현 완료

--- src/main/java/bleuauction/bleuauction_be/server/notice/entity/Notice.java
+++ src/main/java/bleuauction/bleuauction_be/server/notice/entity/Notice.java
@@ -1,57 +1,5 @@
 package bleuauction.bleuauction_be.server.notice.entity;
 
-//import bleuauction.bleuauction_be.server.member.entity.Member;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//import lombok.ToString;
-//import org.hibernate.annotations.CurrentTimestamp;
-//import org.joda.time.DateTime;
-//import org.joda.time.LocalDateTime;
-//
-//import java.sql.Timestamp;
-//
-//@Entity
-//@Getter
-//@Setter
-//@ToString
-//@Table(name = "ba_notice")
-//public class Notice {
-//
-//  @Id
-//  @GeneratedValue(strategy = GenerationType.IDENTITY)
-//  @Column(name="notice_no")
-//  private Long noticeNo;
-//
-//  @Column(name="notice_title")
-//  private String noticeTitle;
-//  @Column(name="notice_content")
-//  private String noticeContent;
-//
-//  @ManyToOne(fetch = FetchType.LAZY)
-//  @JoinColumn(name ="member_no")
-//  private Member member;
-//
-//  @CurrentTimestamp
-//  @Column(name="reg_datetiem")
-//  private LocalDateTime regDatetime;
-//  @CurrentTimestamp
-//  @Column(name="mdf_datetime")
-//  private LocalDateTime mdfDatetime;
-//
-//  @Enumerated(EnumType.STRING)
-//  @Column(name="notice_status")
-//  private NoticeStatus noticeStatus; // 상태 [Y,N]
-//
-//
-//  //비지니스로직
-//  //공지사항 삭제
-//  public void delete(){
-//    this.setNoticeStatus(NoticeStatus.N);
-//  }
-//
-//}
-
 import bleuauction.bleuauction_be.server.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.Getter;

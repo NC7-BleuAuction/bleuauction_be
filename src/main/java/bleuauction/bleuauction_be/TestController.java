e6247c1a7809de49c27aca899a5d4cc4c9f21c2d
shin2012649 <shin2012649@gmail.com>
2023-10-10 04:43:14 +0900

    TestController(Controller -> RestController)수정

--- src/main/java/bleuauction/bleuauction_be/TestController.java
+++ src/main/java/bleuauction/bleuauction_be/TestController.java
@@ -11,13 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
-@Controller
+@RestController
 //@RestController
 public class TestController {
 

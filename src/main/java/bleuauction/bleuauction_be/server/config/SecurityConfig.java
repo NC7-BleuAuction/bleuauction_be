aa89b1cbc1a095b716fb0d4732cd4b9a4bedc520
shin2012649 <shin2012649@gmail.com>
2023-10-12 18:56:40 +0900

    setAllowedOrigins -> http://bleuauction.co.kr 수정

--- src/main/java/bleuauction/bleuauction_be/server/config/SecurityConfig.java
+++ src/main/java/bleuauction/bleuauction_be/server/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
               configuration.setAllowedOriginPatterns(Arrays.asList("*"));
               configuration.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
               configuration.setAllowedHeaders(Arrays.asList("*"));
-              configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+//              configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+              configuration.setAllowedOrigins(List.of("http://bleuauction.co.kr"));
               configuration.setAllowCredentials(true);
 
               UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();

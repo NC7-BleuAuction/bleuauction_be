37531f65dc98e105fd8641765ef6b08a8f04ece3
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-10-13 18:06:13 +0900

    Gihyeon (#45)
    * SQL DDL file update order, ordermenu, pay table relationship resetting
    
    * SQL DDL 최신화 주문 테이블을 주문과 주문메뉴 테이블로 나누어서 정규화 밒 주문, 주문메뉴, 결제 테이블 관계  재설정

--- src/main/java/bleuauction/bleuauction_be/server/attach/entity/Attach.java
+++ src/main/java/bleuauction/bleuauction_be/server/attach/entity/Attach.java
@@ -4,22 +4,25 @@ import bleuauction.bleuauction_be.server.item.entity.Item;
 import bleuauction.bleuauction_be.server.menu.entity.Menu;
 import bleuauction.bleuauction_be.server.notice.entity.Notice;
 import bleuauction.bleuauction_be.server.review.entity.Review;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CurrentTimestamp;
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.UpdateTimestamp;
+import lombok.Setter;
+import org.hibernate.annotations.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Entity
+@Setter
 @Data
 @Table(name = "ba_attach")
 @NoArgsConstructor
 @DynamicInsert
+@DynamicUpdate
 public class Attach {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,9 +33,10 @@ public class Attach {
     @JoinColumn(name = "menuNo")
     private Menu menuNo;
 
+    @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "reviewNo")
-    private Review reviewNo;
+    private Review review;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "itemNo")
@@ -52,12 +56,10 @@ public class Attach {
     @NotNull
     private String saveFilename;
 
-    @CurrentTimestamp
-    @Column(name = "reg_datetime")
+    @CreationTimestamp
     private Timestamp regDatetime;
 
     @UpdateTimestamp
-    @Column(name = "mdf_datetime")
     private Timestamp mdfDatetime;
 
     @Enumerated(EnumType.STRING)

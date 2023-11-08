e9d77c29513e5ea590a71cd6852a91b82da081d2
donsonioc2010 <whddnjs822@gmail.com>
2023-11-08 11:48:57 +0900

    test : test디렉토리

---
+++ src/test/java/bleuauction/bleuauction_be/TestControllerTest.java
@@ -0,0 +1,17 @@
+package bleuauction.bleuauction_be;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class TestControllerTest {
+
+    @Test
+    @DisplayName("응애")
+    void 응애응애() {
+        log.info("오하요 고자이마스");
+    }
+}

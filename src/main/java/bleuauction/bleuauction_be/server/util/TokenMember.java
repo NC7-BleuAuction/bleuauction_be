70b3bf6ea20523aea1a55bb4acb37609791cc239
donsonioc2010 <whddnjs822@gmail.com>
2023-11-16 18:15:35 +0900

    refactor : TokenMember작업내역
    - @Builder추가
    - 사용하지 않는 Import 제거

--- src/main/java/bleuauction/bleuauction_be/server/util/TokenMember.java
+++ src/main/java/bleuauction/bleuauction_be/server/util/TokenMember.java
@@ -1,38 +1,21 @@
 package bleuauction.bleuauction_be.server.util;
 
-import bleuauction.bleuauction_be.server.attach.entity.Attach;
-import bleuauction.bleuauction_be.server.member.entity.Member;
-import bleuauction.bleuauction_be.server.member.entity.MemberCategory;
-import bleuauction.bleuauction_be.server.member.entity.MemberStatus;
-import bleuauction.bleuauction_be.server.notice.entity.Notice;
-import bleuauction.bleuauction_be.server.orderMenu.entity.OrderMenu;
-import bleuauction.bleuauction_be.server.review.entity.Review;
-import bleuauction.bleuauction_be.server.store.entity.Store;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.annotation.Nullable;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.lang.reflect.Field;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 public class TokenMember {
-  private Long memberNo;
+    private Long memberNo;
 
-  private String memberEmail;
+    private String memberEmail;
 
-  private String memberName;
+    private String memberName;
 
-  private String memberCategory;
+    private String memberCategory;
 }
 

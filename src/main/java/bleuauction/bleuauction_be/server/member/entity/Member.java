09e32eee578ff95828a50ca6aca48e6698c5da00
donsonioc2010 <whddnjs822@gmail.com>
2023-11-18 18:16:16 +0900

    feat : MemberEntity변경점
    - 연관관계 없는 필드들로만 구성된 Builder추가

--- src/main/java/bleuauction/bleuauction_be/server/member/entity/Member.java
+++ src/main/java/bleuauction/bleuauction_be/server/member/entity/Member.java
@@ -1,6 +1,7 @@
 package bleuauction.bleuauction_be.server.member.entity;
 
 
+import bleuauction.bleuauction_be.server.answer.entity.Answer;
 import bleuauction.bleuauction_be.server.attach.entity.Attach;
 import bleuauction.bleuauction_be.server.notice.entity.Notice;
 import bleuauction.bleuauction_be.server.orderMenu.entity.OrderMenu;
@@ -17,11 +18,14 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -47,76 +51,91 @@ import java.util.List;
 @Table(name = "ba_member")
 public class Member implements Serializable {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "member_no")
-  private Long memberNo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_no")
+    private Long memberNo;
 
-  @NotNull
-  @Column(name = "member_email", unique = true)
-  private String memberEmail;
+    @NotNull
+    @Column(name = "member_email", unique = true)
+    private String memberEmail;
 
-  @NotNull
-  private String memberPwd;
+    @NotNull
+    private String memberPwd;
 
-  @NotNull
-  private String memberName;
+    @NotNull
+    private String memberName;
 
-  @NotNull
-  private String memberZipcode;
+    @NotNull
+    private String memberZipcode;
 
-  @NotNull
-  private String memberAddr;
+    @NotNull
+    private String memberAddr;
 
-  @NotNull
-  private String memberDetailAddr;
+    @NotNull
+    private String memberDetailAddr;
 
-  @NotNull
-  private String memberPhone;
+    @NotNull
+    private String memberPhone;
 
-  @Nullable
-  private String memberBank;
+    @Nullable
+    private String memberBank;
 
-  @Nullable
-  private String memberAccount;
+    @Nullable
+    private String memberAccount;
 
-  @Enumerated(EnumType.STRING)
-  private MemberCategory memberCategory;
+    @Enumerated(EnumType.STRING)
+    private MemberCategory memberCategory;
 
-  @CreationTimestamp
-  @Column(name = "reg_datetime")
-  private Timestamp regDatetime;
+    @CreationTimestamp
+    @Column(name = "reg_datetime")
+    private Timestamp regDatetime;
 
-  @UpdateTimestamp
-  @Column(name = "mdf_datetime")
-  private Timestamp mdfDatetime;
+    @UpdateTimestamp
+    @Column(name = "mdf_datetime")
+    private Timestamp mdfDatetime;
 
-  @Enumerated(EnumType.STRING)
-  private MemberStatus memberStatus;
+    @Enumerated(EnumType.STRING)
+    private MemberStatus memberStatus;
 
-  @JsonManagedReference
-  @OneToMany(mappedBy = "memberNo")
-  private List<Notice> notices = new ArrayList<>();
+    @JsonManagedReference
+    @OneToMany(mappedBy = "memberNo")
+    private List<Notice> notices = new ArrayList<>();
 
-  @JsonBackReference
-  @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
-  private List<Review> reviews = new ArrayList<>();
+    @JsonBackReference
+    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
+    private List<Review> reviews = new ArrayList<>();
 
-  @JsonManagedReference
-  @OneToMany(mappedBy = "memberNo")
-  private List<OrderMenu> OrderMenus = new ArrayList<>();
+    @JsonManagedReference
+    @OneToMany(mappedBy = "memberNo")
+    private List<OrderMenu> OrderMenus = new ArrayList<>();
 
-  @JsonManagedReference
-  @OneToMany(mappedBy = "memberNo")
-  private List<Attach> memberAttaches = new ArrayList<>();
+    @JsonManagedReference
+    @OneToMany(mappedBy = "memberNo")
+    private List<Attach> memberAttaches = new ArrayList<>();
 
-  @JsonBackReference
-  @OneToMany(mappedBy = "memberNo")
-  private List<Store> ownedStores = new ArrayList<>();
+    @JsonBackReference
+    @OneToMany(mappedBy = "memberNo")
+    private List<Store> ownedStores = new ArrayList<>();
 
-  public void addAttaches(Attach attach) {
-    memberAttaches.add(attach);
-    attach.setMemberNo(this);
-  }
+    public void addAttaches(Attach attach) {
+        memberAttaches.add(attach);
+        attach.setMemberNo(this);
+    }
+
+    @Builder
+    public Member(String memberEmail, String memberPwd, String memberName, String memberZipcode, String memberAddr, String memberDetailAddr, String memberPhone, @Nullable String memberBank, @Nullable String memberAccount, MemberCategory memberCategory, MemberStatus memberStatus) {
+        this.memberEmail = memberEmail;
+        this.memberPwd = memberPwd;
+        this.memberName = memberName;
+        this.memberZipcode = memberZipcode;
+        this.memberAddr = memberAddr;
+        this.memberDetailAddr = memberDetailAddr;
+        this.memberPhone = memberPhone;
+        this.memberBank = memberBank;
+        this.memberAccount = memberAccount;
+        this.memberCategory = memberCategory;
+        this.memberStatus = memberStatus;
+    }
 }
 

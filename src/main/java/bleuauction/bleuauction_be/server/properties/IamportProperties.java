cdd0b32385445790b209f22e5e01e1bd37853104
donsonioc2010 <whddnjs822@gmail.com>
2023-11-23 22:36:50 +0900

    feat : Pay 수정사항
    - IamportProperties생성
        - application-pay의 iamport 항목을 가져오는 Properties
    - ConfigurationProperties
        - IamportProperties Bean으로 생성이 되도록 Class등록
    - PayConfig
        - @Value제거 및 IamportProperties 주입하도록 방식 변경
        - Bean 생성 방식에 대해서, Properties에 주입된 값을 사용하도록 변경
    - application-pay.yml
        - 문자열...쌍따옴표 제거 (불편해서)

---
+++ src/main/java/bleuauction/bleuauction_be/server/properties/IamportProperties.java
@@ -0,0 +1,13 @@
+package bleuauction.bleuauction_be.server.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "iamport")
+public class IamportProperties {
+    private String key;
+    private String secret;
+}

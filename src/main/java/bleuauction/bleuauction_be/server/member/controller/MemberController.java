6a5710d1e9fa78c46495641213399ddfe565c0db
donsonioc2010 <whddnjs822@gmail.com>
2023-11-23 10:53:10 +0900

    refactor : 누락된 API메소드 변경
    - 회원정보 수정 POST -> PUT

--- src/main/java/bleuauction/bleuauction_be/server/member/controller/MemberController.java
+++ src/main/java/bleuauction/bleuauction_be/server/member/controller/MemberController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -140,14 +141,12 @@ public class MemberController {
      * @return
      * @throws Exception
      */
-    @PostMapping("/update")
+    @PutMapping
     public ResponseEntity<?> updateMember(
             @RequestHeader("Authorization") String authorizationHeader,
             @RequestBody UpdateMemberRequest updateMemberRequest) throws Exception {
         // TODO : JWT 검증기능이며 추후 기현님 작업완료되면 변경 예정
-        ResponseEntity<?> verificationResult = createJwt.verifyAccessToken(
-                authorizationHeader,
-                createJwt);
+        ResponseEntity<?> verificationResult = createJwt.verifyAccessToken(authorizationHeader, createJwt);
         if (verificationResult != null) {
             return verificationResult;
         }

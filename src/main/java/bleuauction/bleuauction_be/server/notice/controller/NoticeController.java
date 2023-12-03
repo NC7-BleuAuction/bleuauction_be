c456b3681c70994943acbadac8fb1ccfec2ca93a
seunghyun333 <seunghyun@bagseunghyeon-ui-MacBookPro.local>
2023-12-04 00:07:08 +0900

    [Refactor, Test] Notice 도메인 작업 #113 컨트롤러 -> 서비스 수정

--- src/main/java/bleuauction/bleuauction_be/server/notice/controller/NoticeController.java
+++ src/main/java/bleuauction/bleuauction_be/server/notice/controller/NoticeController.java
@@ -36,7 +36,6 @@ public class NoticeController {
   private final NoticeService noticeService;
   private final MemberService memberService;
   private final CreateJwt createJwt;
-  private final NcpObjectStorageService ncpObjectStorageService;
   private final AttachService attachService;
 
 
@@ -44,31 +43,18 @@ public class NoticeController {
   // 등록 처리(관리자 회원)
   @PostMapping("/new")
   @Transactional
-  public ResponseEntity<?>  notice(@RequestHeader("Authorization") String  authorizationHeader, Notice notice, @RequestParam(name = "multipartFiles",required = false) List<MultipartFile> multipartFiles) {
+  public ResponseEntity<?>  notice(@RequestHeader("Authorization") String  authorizationHeader, Notice notice, @RequestParam(name = "multipartFiles",required = false) List<MultipartFile> multipartFiles) throws Exception{
 
     createJwt.verifyAccessToken(authorizationHeader);
     TokenMember tokenMember = createJwt.getTokenMember(authorizationHeader);
     Optional<Member> loginUser = memberService.findByMemberNo(tokenMember.getMemberNo());
 
-    if(loginUser.get().getMemberCategory() == A) {
-    notice.setMemberNo(loginUser.get());
-
-    if (multipartFiles != null && multipartFiles.size() > 0) {
-      ArrayList<Attach> attaches = new ArrayList<>();
-      for (MultipartFile multipartFile : multipartFiles) {
-        if (multipartFile.getSize() > 0) {
-          Attach attach = ncpObjectStorageService.uploadFile(new Attach(),
-                  "bleuauction-bucket", "notice/", multipartFile);
-          notice.addNoticeAttach(attach);
-        }
-      }
-    }
-
-    noticeService.enroll(notice);
+    noticeService.enroll(notice,multipartFiles,loginUser.get());
     log.info("notice/postnew");
 
-    return ResponseEntity.status(HttpStatus.CREATED).body("Notice created successfully");}
-    else {
+    try {
+      return ResponseEntity.status(HttpStatus.CREATED).body("Notice created successfully");
+    } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("관리자 권한이 필요합니다");
     }
   }
@@ -76,45 +62,21 @@ public class NoticeController {
 
   //목록조회
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Notice> findNotices() throws Exception {
-    try {
+  public List<Notice> findNotices(){
       List<Notice> notices = noticeService.findNoticesByStatus(NoticeStatus.Y);
       return notices;
-    } catch (Exception e) {
-      // 예외 처리 코드 추가
-      e.printStackTrace();
-      return new ArrayList<>();
-    }
   }
 
 
 // 삭제
   @DeleteMapping("/{noticeNo}")
   public ResponseEntity<?> deleteNotice(@RequestHeader("Authorization") String  authorizationHeader, @PathVariable("noticeNo") Long noticeNo) {
-    Notice notice = noticeService.findOne(noticeNo);
-
     createJwt.verifyAccessToken(authorizationHeader);
     TokenMember tokenMember = createJwt.getTokenMember(authorizationHeader);
     Optional<Member> loginUser = memberService.findByMemberNo(tokenMember.getMemberNo());
 
-    if(loginUser.get().getMemberCategory() == A) {
-
-      // 사진 상태를 'N'으로 변경
-      if (notice != null) {
-        if (notice.getNoticeAttaches() != null && !notice.getNoticeAttaches().isEmpty()) {
-          for (Attach attach : notice.getNoticeAttaches()) {
-            attachService.changeFileStatusToDeleteByFileNo(attach.getFileNo());
-          }
-        }
-        noticeService.deleteNotice(noticeNo);
-        return ResponseEntity.ok("Notice deleted successfully");
-      } else {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Notice not found");
-
-      }
-    } else {
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("관리자 권한이 필요합니다");
-    }
+    noticeService.deleteNotice(noticeNo,loginUser.get());
+    return ResponseEntity.ok("Notice deleted successfully");
   }
 
   //사진삭제
@@ -132,56 +94,27 @@ public class NoticeController {
     }
   }
 
-
-
   //디테일
   @GetMapping("/detail/{noticeNo}")
-  public ResponseEntity<Notice> detailNotice(@PathVariable("noticeNo") Long noticeNo) {
-    Notice notice = noticeService.findOne(noticeNo);
-
-    if (notice != null) {
-      notice.setNoticeAttaches(notice.getNoticeAttaches());
-      return ResponseEntity.ok(notice);
-    } else {
-      return ResponseEntity.notFound().build();
-    }
+  public Notice detailNotice(@PathVariable("noticeNo") Long noticeNo) {
+    return noticeService.findOne(noticeNo);
   }
 
   // 수정 처리
   @PutMapping("/update/{noticeNo}")
   public ResponseEntity<?> updateNotice(@RequestHeader("Authorization") String  authorizationHeader,
           @PathVariable("noticeNo") Long noticeNo,
-           @RequestParam(name = "noticeTitle") String noticeTitle,
-           @RequestParam(name = "noticeContent") String noticeContent,
-          @RequestParam(name = "multipartFiles",required = false) List<MultipartFile> multipartFiles) {
-
+          @RequestParam(name = "multipartFiles",required = false) List<MultipartFile> multipartFiles) throws Exception {
     Notice updatedNotice = noticeService.findOne(noticeNo);
 
     createJwt.verifyAccessToken(authorizationHeader);
     TokenMember tokenMember = createJwt.getTokenMember(authorizationHeader);
     Optional<Member> loginUser = memberService.findByMemberNo(tokenMember.getMemberNo());
 
-    if(loginUser.get().getMemberCategory() == A) {
+    noticeService.update(updatedNotice,loginUser.get(),multipartFiles);
 
-      if (multipartFiles != null && multipartFiles.size() > 0) {
-        for (MultipartFile multipartFile : multipartFiles) {
-          if (multipartFile.getSize() > 0) {
-            Attach attach = ncpObjectStorageService.uploadFile(new Attach(),
-                    "bleuauction-bucket", "notice/", multipartFile);
-            updatedNotice.addNoticeAttach(attach);
-          }
-        }
-      }
-      // 공지사항 정보 업데이트
-      updatedNotice.setNoticeTitle(noticeTitle);
-      updatedNotice.setNoticeContent(noticeContent);
+    log.info("notice/update");
+    return ResponseEntity.ok("Notice updated successfully");
 
-      noticeService.update(updatedNotice);
-
-      log.info("notice/update");
-      return ResponseEntity.ok("Notice updated successfully");
-    } else {
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("관리자 권한이 필요합니다");
-    }
   }
 }
--- src/main/java/bleuauction/bleuauction_be/server/notice/service/NoticeService.java
+++ src/main/java/bleuauction/bleuauction_be/server/notice/service/NoticeService.java
@@ -1,13 +1,24 @@
 package bleuauction.bleuauction_be.server.notice.service;
 
+import bleuauction.bleuauction_be.server.attach.entity.Attach;
+import bleuauction.bleuauction_be.server.attach.service.AttachService;
+import bleuauction.bleuauction_be.server.member.entity.Member;
+import bleuauction.bleuauction_be.server.member.entity.MemberCategory;
+import bleuauction.bleuauction_be.server.ncp.NcpObjectStorageService;
 import bleuauction.bleuauction_be.server.notice.entity.Notice;
 import bleuauction.bleuauction_be.server.notice.entity.NoticeStatus;
 import bleuauction.bleuauction_be.server.notice.repository.NoticeRepository1;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static bleuauction.bleuauction_be.server.member.entity.MemberCategory.A;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,11 +26,30 @@ import java.util.List;
 public class NoticeService {
 
   private final NoticeRepository1 noticeRepository;
+  private final NcpObjectStorageService ncpObjectStorageService;
+  private final AttachService attachService;
 
   @Transactional
-  public Long enroll(Notice notice) {
-    noticeRepository.save(notice);
+  public Long enroll(Notice notice, List<MultipartFile> multipartFiles, Member member) {
+
+    if(member.getMemberCategory() == MemberCategory.A) {
+      notice.setMemberNo(member);
+      noticeRepository.save(notice);
+
+      if (multipartFiles != null && multipartFiles.size() > 0) {
+        ArrayList<Attach> attaches = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+          if (multipartFile.getSize() > 0) {
+            Attach attach = ncpObjectStorageService.uploadFile(new Attach(),
+                    "bleuauction-bucket", "notice/", multipartFile);
+            notice.addNoticeAttach(attach);
+          }
+        }
+      }
+
+    }
     return notice.getNoticeNo();
+
   }
 
   //노티스 전체 조회
@@ -41,19 +71,43 @@ public class NoticeService {
 
   //노티스 삭제(N)
   @Transactional
-  public void deleteNotice(Long noticeNo) {
-    Notice notice = noticeRepository.findByNoticeNo(noticeNo);
-    notice.delete();
+  public void deleteNotice(Long noticeNo, Member member) {
+    if (member.getMemberCategory()==MemberCategory.A) {
+      Notice notice = noticeRepository.findByNoticeNo(noticeNo);
+      notice.delete();
+      if (notice.getNoticeAttaches() != null && !notice.getNoticeAttaches().isEmpty()) {
+        for (Attach attach : notice.getNoticeAttaches()) {
+          attachService.changeFileStatusToDeleteByFileNo(attach.getFileNo());
+        }
+      }
+    }
   }
 
   //노티스 수정
   @Transactional
-  public Notice update(Notice notice) {
-    Notice updatenotice = noticeRepository.findByNoticeNo(notice.getNoticeNo());
+  public Notice update(Notice updatedNotice, Member member, List<MultipartFile> multipartFiles) throws Exception{
 
-    updatenotice.setNoticeTitle(notice.getNoticeTitle());
-    updatenotice.setNoticeContent(notice.getNoticeContent());
-    return updatenotice;
+    Notice existingnotice = noticeRepository.findByNoticeNo(updatedNotice.getNoticeNo());
+
+    if(member.getMemberCategory() == A) {
+
+      existingnotice.setNoticeTitle(updatedNotice.getNoticeTitle());
+      existingnotice.setNoticeContent(updatedNotice.getNoticeContent());
+
+      if (multipartFiles != null && multipartFiles.size() > 0) {
+        for (MultipartFile multipartFile : multipartFiles) {
+          if (multipartFile.getSize() > 0) {
+            Attach attach = ncpObjectStorageService.uploadFile(new Attach(),
+                    "bleuauction-bucket", "notice/", multipartFile);
+            existingnotice.addNoticeAttach(attach);
+          }
+        }
+      }
+      return noticeRepository.save(existingnotice);
+    } else {
+      throw new IllegalArgumentException("수정 권한이 없습니다.");
+    }
+
   }
 
 

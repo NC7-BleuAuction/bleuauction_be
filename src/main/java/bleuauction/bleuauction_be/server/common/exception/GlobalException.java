57844b107799fd8c0c0d322606fea6309488678a
donsonioc2010 <whddnjs822@gmail.com>
2023-12-22 14:40:04 +0900

    fix : Menu 누락된 내용추가
    - Exception 항목 추가

--- src/main/java/bleuauction/bleuauction_be/server/common/exception/GlobalException.java
+++ src/main/java/bleuauction/bleuauction_be/server/common/exception/GlobalException.java
@@ -2,6 +2,7 @@ package bleuauction.bleuauction_be.server.common.exception;
 
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import bleuauction.bleuauction_be.server.common.dto.ErrorDetail;
@@ -15,7 +16,8 @@ public enum GlobalException implements BaseErrorCode {
     INVALID_TOKEN_EXCEPTION(UNAUTHORIZED.value(), "I", "올바르지 않은 토큰입니다."),
     RENEW_ACCESS_TOKEN_EXCEPTION(UNAUTHORIZED.value(), "RATE", "AccesToken 재발급 에러입니다."),
     INTERNAL_SERVER_ERRORS(INTERNAL_SERVER_ERROR.value(), "SE", "서버 내부 오류입니다."),
-    FORBIDDEN_ACCESS_EXCEPTION(FORBIDDEN.value(), "403-1", "내부 서버 403 오류 입니다.");
+    FORBIDDEN_ACCESS_EXCEPTION(FORBIDDEN.value(), "403-1", "내부 서버 403 오류 입니다."),
+    MENU_NOTFOUND_EXCEPTION(NOT_FOUND.value(), "404-01", "메뉴를 찾을 수 없습니다.");
 
     private final Integer statusCode;
     private final String errorCode;
